2add7bc07e009d5779996d0f4b8d7db2f80c99c4
suzumura <suz.redmine@ymail.plala.or.jp>
2012-06-25 15:09:53 +0900

    add inject.js for tumblr.

---
+++ inject.js
@@ -0,0 +1,4 @@
+(function() {
+  var s = "http://smokecheese.dyndns.org/css3d_cubic_panorama/forest.html";
+  document.write('<iframe src="' + s + '" width="500px" height="380px"></iframe>');
+})();

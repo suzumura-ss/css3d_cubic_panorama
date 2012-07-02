0f9a90807d3bb31965da64eb10d49cdb1b1fba90
suzumura <suz.redmine@ymail.plala.or.jp>
2012-07-02 11:02:23 +0900

    Add 6-textures sample.

---
+++ injectBadApple6.js
@@ -0,0 +1,4 @@
+(function() {
+  var s = "http://smokecheese.dyndns.org/css3d_cubic_panorama/badApple6.html";
+  document.write('<iframe src="' + s + '" width="500px" height="380px"></iframe>');
+})();
